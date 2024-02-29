fa015865e27f749e915b5ef89dedb85bf29747dc
german amezquita <german.amezquita01@uptc.edu.co>
2024-02-29 16:34:42 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/classworkdinamic/ClassworkdinamicApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.classworkdinamic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassworkdinamicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassworkdinamicApplication.class, args);
+	}
+
+}
